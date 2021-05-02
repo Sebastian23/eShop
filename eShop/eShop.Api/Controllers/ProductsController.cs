b8dbd9dc1881eb55b5d99fd5140611353b047e66
Sebastian Farcas <sebastian.farcas@tecss.com>
2021-05-02 15:03:49 +0300

    Added Docker support

---
+++ eShop/eShop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var prod1 = new {Name = "Laptop", Price = 100, Color = "Red"};
+            var prod2 = new {Name = "PC", Price = 23, Color = "Blue"};
+            var prod3 = new {Name = "Mouse", Price = 432, Color = "Black"};
+            var products = new List<object>{prod1, prod2, prod3};
+            return Ok(products);
+        }
+    }
+}
